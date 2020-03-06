3f6c4e220fbf4f64041b383997af29e63f103d37
Jon Stokes <jon@jonstokes.com>
2020-03-06 12:32:30 -0600

    Getting input values

--- src/Controls.js
+++ src/Controls.js
@@ -1,8 +1,20 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { InputGroup, FormControl } from 'react-bootstrap'
 
-export default class Controls extends PureComponent {
-  render() {
+export default class Controls extends Component {
+  constructor() {
+    /* 1. Initialize Ref */
+    super(); 
+    this.textInput = React.createRef(); 
+ }
+
+ handleChange() {
+    /* 3. Get Ref Value here (or anywhere in the code!) */
+    const value = this.textInput.current.value
+    console.log(value)
+ }
+
+ render() {
     return (
       <div style={{ paddingTop: '2rem' }}>
         <InputGroup className="mb-3">
@@ -10,6 +22,8 @@ export default class Controls extends PureComponent {
             <InputGroup.Text id="inputGroup-sizing-default">Length of stay</InputGroup.Text>
           </InputGroup.Prepend>
           <FormControl
+            ref={this.textInput}
+            onChange={() => this.handleChange()}
             aria-label="Default"
             aria-describedby="inputGroup-sizing-default"
           />
@@ -25,7 +39,40 @@ export default class Controls extends PureComponent {
             aria-describedby="inputGroup-sizing-default"
           />
         </InputGroup>
-      </div>    
+        <br />
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+          <InputGroup.Text>%</InputGroup.Text>
+            <InputGroup.Text id="inputGroup-sizing-default">Hospitalization rate</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+        <br />
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+          <InputGroup.Text>%</InputGroup.Text>
+            <InputGroup.Text id="inputGroup-sizing-default">ICU rate</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+        <br />
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+          <InputGroup.Text>%</InputGroup.Text>
+            <InputGroup.Text id="inputGroup-sizing-default">Case fatality rate</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+      </div>
     );
   }
 }
