329bd117e9c39613237a593b5e3dde1f44e79112
Jon Stokes <jon@jonstokes.com>
2020-03-06 14:54:56 -0600

    Fixes

--- src/Chart.js
+++ src/Chart.js
@@ -43,13 +43,45 @@ const data = [
 
 ];
 
+function stdNormalDistribution (x) {
+  return Math.pow(Math.E,-Math.pow(x,2)/2)/Math.sqrt(2*Math.PI);
+}
+
+//Change to props soon
+const lengthOfOutbreak = 10;
+
 export default class Example extends PureComponent {
+
+  computeNormalForDay(day) {
+    const midpoint = lengthOfOutbreak / 2;
+    let x;
+
+    if(day >= midpoint) {
+      x = 1 - day
+    } else {
+      x = -1 - day
+    }
+    return(stdNormalDistribution(day / lengthOfOutbreak) * 10)
+  }
+
+  generateData() {
+    const dayRange = [...Array(lengthOfOutbreak).keys()];
+    const bedData = dayRange.map((day) => {
+      return({ name: day, hospital: this.computeNormalForDay(day), icu: this.computeNormalForDay(day) })
+    });
+
+    return bedData
+  }
+
   render() {
+    const bedData = this.generateData();
+    console.log(bedData);
+
     return (
       <AreaChart
         width={500}
         height={400}
-        data={data}
+        data={bedData}
         margin={{
           top: 10, right: 30, left: 0, bottom: 0,
         }}
@@ -58,9 +90,8 @@ export default class Example extends PureComponent {
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
-        <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
-        <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-        <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        <Area type="monotone" dataKey="hospital" stackId="1" stroke="#8884d8" fill="#8884d8" />
+        <Area type="monotone" dataKey="icu" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
       </AreaChart>
     );
   }
